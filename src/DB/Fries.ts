7775a6570ddbc76108718058eb8f4f07aeff7a5f
Juan Martin Lopez Frau <juanmartinlopezfrau@gmail.com>
2025-09-11 22:42:20 -0300

    Update prices for fries in Fries.ts

--- src/DB/Fries.ts
+++ src/DB/Fries.ts
@@ -2,13 +2,13 @@ const fries = {
   1: {
     id: 1,
     name: "Papa fritas Chicas",
-    price: 4500,
+    price: 5200,
     image: "",
   },
   2: {
     id: 2,
     name: "Papa fritas Grandes",
-    price: 5500,
+    price: 6200,
     image: "",
   },
 };
