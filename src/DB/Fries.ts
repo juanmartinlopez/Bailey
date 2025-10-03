b45819965a8f95a52be0d1793495987d217dfade
Juan Martin Lopez <juanmartinlopezfrau@gmail.com>
2025-10-03 20:44:49 -0300

    papas nuevas

--- src/DB/Fries.ts
+++ src/DB/Fries.ts
@@ -11,6 +11,18 @@ const fries = {
     price: 6200,
     image: "",
   },
+  3: {
+    id: 3,
+    name: "Papa fritas con cheddar & panceta Chicas",
+    price: 7500,
+    image: "",
+  },
+  4: {
+    id: 4,
+    name: "Papa fritas con cheddar & panceta Grandes",
+    price: 8500,
+    image: "",
+  },
 };
 
 export default fries;
